494d170b72b956df0c18f95c747661c937accf11
Anthony Zhao <anthonyrzhao@gmail.com>
2021-10-16 12:48:06 -0400

    LinReg Doesn't Work

--- winnerDistribution.py
+++ winnerDistribution.py
@@ -1,15 +1,16 @@
+import numpy
+
 import gameClass
 from matplotlib import pyplot as plt
 import copy
 import collections
 
-playerCount = 10
+playerCount = 50
 tokenCount = 1
 
 rawLen = []
 rawWin = []
-gameNumber = 50000
-
+gameNumber = 10000
 
 for i in range(gameNumber):
     tempGame = gameClass.game(playerCount, tokenCount)
@@ -21,19 +22,20 @@ for i in range(gameNumber):
 rawLen = collections.Counter(rawLen)
 rawWin = collections.Counter(rawWin)
 
-print (rawWin)
+print(rawWin)
 
-plt.scatter(rawLen.keys(),rawLen.values(),marker='.')
+plt.scatter(rawLen.keys(), rawLen.values(), marker='.')
 plt.xlabel('Number of Turns')
 plt.ylabel('Amount of Games')
 plt.show()
 
-
 winPercent = []
 for i in rawWin.keys():
-    winPercent.append(rawWin[i]*100/gameNumber)
+    winPercent.append(rawWin[i] * 100 / gameNumber)
 
-plt.scatter(rawWin.keys(), winPercent, marker = '.')
+plt.scatter(rawWin.keys(), winPercent, marker='.')
+m, b = numpy.polyfit(rawWin.keys(), winPercent, 1)
+plt.plot(rawWin.keys(), m*rawWin.keys() + b)
 plt.ylabel('Win Percent')
 plt.xlabel('Player #')
 plt.show()
